ac67f9f97ea631d99acdbffa5fc4f95ec4aee89c
tadeogutierrez <tadeogutierrezsanchez@gmail.com>
2016-01-05 15:46:44 -0600

    kregisterscriptsfolder

--- scripts/kregisterscriptsfolder.sh
+++ scripts/kregisterscriptsfolder.sh
@@ -4,7 +4,7 @@ main() {
 filelines=$(ls /opt/kalan/scripts)
 for line in $filelines ; do
     #echo "Creando link para script $line"
-	/opt/kalan/scripts/kalan-registrar-script.sh /opt/kalan/scripts/$line
+	/opt/kalan/scripts/kregisterscript.sh /opt/kalan/scripts/$line
 done
 
 }
