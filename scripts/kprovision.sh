83eee882f63f038696708386ba75d14ea15fd3c0
tadeogutierrez <tadeogutierrezsanchez@gmail.com>
2016-01-06 13:02:10 -0600

    kprovision applications

--- scripts/kprovision.sh
+++ scripts/kprovision.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-#/opt/kalan/scripts/kbuild.sh
+#/opt/kalan/scripts/kprovision.sh
 main() {
-   imagetag="$1";shift;
-   param2="$1";shift;
-   if [[ -d /opt/kalan/dockerfiles/$imagetag ]];then
-      cd /opt/kalan/dockerfiles/$imagetag
-      docker build -t $imagetag .
+   KALAN_PROVISIONS_DIR="/opt/kalan-data/provisions"
+   provisionname="$1";shift;
+   src_w2papps="$1";shift;
+   if [[ ! -d $KALAN_PROVISIONS_DIR/$provisionname ]];then
+      if [[ -n "$src_w2papps" ]];then
+         if [[ -e $src_w2papps/__init__.py ]];then
+            mkdir -p $KALAN_PROVISIONS_DIR/$provisionname/applications
+            cp -r $src_w2papps $KALAN_PROVISIONS_DIR/$provisionname/applications
+         else
+            echo "There is no valid w2p apps folder at $src_w2papps"
+         fi
+      else
+         mkdir -p $KALAN_PROVISIONS_DIR/$provisionname/applications
+      fi
+
+      cd $KALAN_PROVISIONS_DIR/$provisionname
+
    else
-      echo "Must provide a valid folder name in /opt/kalan/dockerfiles with valid dockerfile"
-      echo "folder:$imagetag"
-      echo "$param2"
+      echo "There is previous provision with name $provisionname"
+      echo "folder: $KALAN_PROVISIONS_DIR/$provisionname"
+      ls $KALAN_PROVISIONS_DIR
+      echo "apps folder: $src_w2papps"
    fi
 }
 
