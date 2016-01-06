92d68e819523398e212db147fb3a930a6495032f
tadeogutierrez <tadeogutierrezsanchez@gmail.com>
2016-01-06 01:36:13 -0600

    force commit

---
+++ scripts/kbuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#/opt/kalan/scripts/kbuild.sh
+main() {
+   dockerfile="$1";shift;
+   param2="$1";shift;
+   if [[ -d /opt/kalan/dockerfiles/$dockerfile ]];then
+      cd /opt/kalan/dockerfiles/$dockerfile
+      docker build -t $dockerfile .
+   else
+      echo "Must provide a valid folder name in /opt/kalan/dockerfiles with valid dockerfile"
+      echo $param2
+   fi
+}
+
+main "$@"
