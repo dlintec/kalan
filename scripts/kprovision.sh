c0b9a45d570de747658603ec274877441a1d98d2
tadeogutierrez <tadeogutierrezsanchez@gmail.com>
2016-01-06 13:51:59 -0600

    added kcleandockervolumes.sh

--- scripts/kprovision.sh
+++ scripts/kprovision.sh
@@ -4,20 +4,28 @@ main() {
    KALAN_PROVISIONS_DIR="/opt/kalan-data/provisions"
    provisionname="$1";shift;
    src_w2papps="$1";shift;
+   provisioncreated=false;
+   provision_applications=$KALAN_PROVISIONS_DIR/$provisionname/applications
    if [[ ! -d $KALAN_PROVISIONS_DIR/$provisionname ]];then
       if [[ -n "$src_w2papps" ]];then
          if [[ -e $src_w2papps/__init__.py ]];then
-            mkdir -p $KALAN_PROVISIONS_DIR/$provisionname/applications
-            cp -r $src_w2papps $KALAN_PROVISIONS_DIR/$provisionname/applications
+            mkdir -p $provision_applications
+            cp -r $src_w2papps $provision_applications
+            provisioncreated=true;
          else
             echo "There is no valid w2p apps folder at $src_w2papps"
          fi
       else
-         mkdir -p $KALAN_PROVISIONS_DIR/$provisionname/applications
+         mkdir -p $provision_applications
+         provisioncreated=true;
       fi
-
+      if [[ provisioncreated ]];then
+         docker create \
+            -v $provision_applications:/opt/k-w2p/web2py/applications \
+            --name $provisionname-provision ubuntu
+      fi
    else
-      echo "There is previous provision with name $provisionname"
+      echo "There is previous prevision with name $provisionname"
       echo "folder: $KALAN_PROVISIONS_DIR/$provisionname"
       ls $KALAN_PROVISIONS_DIR
       echo "apps folder: $src_w2papps"
