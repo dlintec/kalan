005fe54c4dd57c06635b66264b4995e96070c767
tadeogutierrez <tadeogutierrezsanchez@gmail.com>
2016-01-05 23:29:10 -0600

    install-docker fix

--- scripts/kalan-install-docker.sh
+++ scripts/kalan-install-docker.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+#/opt/kalan/scripts/kalan-install-docker.sh
 source /opt/kalan/scripts/kalan-lib.sh
 PACKAGE_MANAGER=$(get_package_manager)
 curl -sSL https://get.docker.com/ | sh
-PACKAGE_MANAGER -y upgrade docker-engine
+$PACKAGE_MANAGER -y upgrade docker
 service docker start
 systemctl enable docker
+service docker enable
 curl -L https://github.com/docker/machine/releases/download/v0.5.3/docker-machine_linux-amd64 >/usr/local/bin/docker-machine && \
 chmod +x /usr/local/bin/docker-machine
 curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
