a45c4eb9a26fac09b67cf16c721f1dbdf42d8249
tadeogutierrez <tadeogutierrezsanchez@gmail.com>
2016-01-08 01:38:37 -0600

    kregisterscriptsfolder

--- kalan-setup.sh
+++ kalan-setup.sh
@@ -84,7 +84,7 @@ chmod +x ~/kalan/src/kalan-update.sh
 chmod +x ~/kalan/src/kregisterscript.sh
 chmod +x ~/kalan/src/kregisterscriptsfolder.sh
 cd ~/kalan/src/
-./cd ..kregisterscriptsfolder.sh
+./kregisterscriptsfolder.sh
 echo "export PATH=$PATH:~/kalan/bin"  >> ~/.bash_profile
 echo "export PATH=$PATH:~/kalan/bin"  >> ~/.bashrc
 
