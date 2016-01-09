26c004663eb73653a4bee8abd079e510837c3805
tadeogutierrez <tadeogutierrezsanchez@gmail.com>
2016-01-09 01:24:43 -0800

    containers working ok !!!!!!

--- src/kprovision.sh
+++ src/kprovision.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 #~/kalan/src/kprovision.sh
 main() {
-   KALAN_PROVISIONS_DIR="~/kalan-data/provisions"
+  KALAN_USER="$(who am i | awk '{print $1}')"
+  KALAN_DIR="/home/$KALAN_USER/kalan"
+
+   KALAN_PROVISIONS_DIR="$KALAN_DIR-data/provisions"
    provisionname="$1";shift;
    src_w2papps="$1";shift;
    provisioncreated=false;
    container_appfolder="/var/kalan-container/web2py/applications"
    if [[ ! -d $KALAN_PROVISIONS_DIR/$provisionname ]];then
       if [[ -z "$src_w2papps" ]];then
-         src_w2papps="~/kalan/dockerfiles/k-w2p/kalan-container/web2py/applications"
+         src_w2papps="$KALAN_DIR/dockerfiles/k-w2p/kalan-container/web2py/applications"
       fi
       if [[ -e $src_w2papps/__init__.py ]];then
          provision_appfolder=$KALAN_PROVISIONS_DIR/$provisionname/applications
          mkdir -p $provision_appfolder
          cp -rf $src_w2papps $KALAN_PROVISIONS_DIR/$provisionname/
-         docker create \
+         sudo docker create \
             -v $provision_appfolder:$container_appfolder \
             --name $provisionname-provision ubuntu:14.04.3
             if [ $? -eq 0 ]; then
-               docker run -p 8443:8443 -p 8888:8888 \
+               sudo docker run -p 8443:8443 -p 8888:8888 \
                   --volumes-from $provisionname-provision -d \
 		            --entrypoint /usr/bin/python \
                   --name $provisionname \
                   k-w2p \
-                  ~/kalan-container/web2py/web2py.py --nogui -i 0.0.0.0 -p 8888 -a "<recycle>"
+                  /var/kalan-container/web2py/web2py.py --nogui -i 0.0.0.0 -p 8888 -a "<recycle>"
 
-	            docker exec $provisionname chown -R kalan:kalan ~/kalan-container/web2py/applications
+	            sudo docker exec $provisionname chown -R kalan:kalan /var/kalan-container/web2py/applications
             else
                echo "Failed creating new provision for data container: $provisionname-provision"
             fi
@@ -38,11 +41,11 @@ main() {
    else
       if [[ "$src_w2papps" == "--remove" ]];then
          echo "removin provision $provisionname"
-         docker stop $provisionname
-         docker rm -v $provisionname
-         docker rm -v $provisionname-provision
+         sudo docker stop $provisionname
+         sudo docker rm -v $provisionname
+         sudo docker rm -v $provisionname-provision
          if [ -d $KALAN_PROVISIONS_DIR/$provisionname ];then
-            rm -rf $KALAN_PROVISIONS_DIR/$provisionname
+            sudo rm -rf $KALAN_PROVISIONS_DIR/$provisionname
          else
             echo "error removing provision $KALAN_PROVISIONS_DIR/$provisionname"
          fi
