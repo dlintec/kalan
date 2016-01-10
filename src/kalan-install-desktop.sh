4b5c9a295b3d590c92c4b75632ee8f04c14d3bb5
tadeogutierrez <tadeogutierrezsanchez@gmail.com>
2016-01-10 02:14:45 -0800

    removed icons to data folder

--- src/kalan-install-desktop.sh
+++ src/kalan-install-desktop.sh
@@ -2,7 +2,7 @@ param1="$1";shift
 
 KALAN_USER="$(who am i | awk '{print $1}')"
 KALAN_DIR="$HOME/kalan"
-
+current_dir=`pwd`
 if [[ ! -d $KALAN_DIR-data/downloads ]];then
     mkdir -p $KALAN_DIR-data/downloads
 fi
@@ -12,21 +12,44 @@ atom_ver="v1.3.3"
 if [[ ! -e $KALAN_DIR-data/downloads/atom-amd64.deb.$atom_ver ]];then
     wget https://atom-installer.github.com/$atom_ver/atom-amd64.deb
     mv $KALAN_DIR-data/downloads/atom-amd64.deb $KALAN_DIR-data/downloads/atom-amd64.deb.$atom_ver
-
+    sudo dpkg -i $KALAN_DIR-data/downloads/atom-amd64.deb.$atom_ver
 fi
 
-sudo add-apt-repository ppa:nemh/systemback
-sudo apt-get -y update
-sudo apt-get -y install systemback
 
-sudo dpkg -i $KALAN_DIR-data/downloads/atom-amd64.deb.$atom_ver
+if ! [ -x "$(command -v systemback-cli)" ]; then
+  echo "-------------------------------------------------------------------------"
+  echo "   systemback is not installed"
+  echo "   We will use: sudo $PACKAGE_MANAGER install systemback"
+  echo "-------------------------------------------------------------------------"
+  sudo add-apt-repository ppa:nemh/systemback
+  sudo apt-get -y update
+  sudo apt-get -y install systemback
+fi
+
+
+
 sudo apt-get -y install xfce4 synaptic glade aptitude unity-tweak-tool
 
 
+cd $KALAN_DIR-data/downloads
+git clone --recursive https://github.com/moka-project/faba-icon-theme.git
+cd $KALAN_DIR-data/downloads/moka-project
+git pull
+sudo cp -rf $KALAN_DIR-data/downloads/faba-icon-theme/Faba /usr/share/icons/Faba
 
+cd $KALAN_DIR-data/downloads
+git clone --recursive https://github.com/moka-project/moka-icon-theme.git
+cd $KALAN_DIR-data/downloads/moka-project
+git pull
+bash autogen.sh
+make
+sudo make install
 
-sudo cp -rf $KALAN_DIR/sw/icons/Moka /usr/share/icons/
-sudo cp -rf $KALAN_DIR/sw/icons/Faba /usr/share/icons/
+cp /usr/share/icons/Faba cd $KALAN_DIR-data/downloads/Faba
+cp /usr/share/icons/Moka cd $KALAN_DIR-data/downloads/Moka
+
+cd $current_dir
+
 
 #sudo cp -rf $KALAN_DIR/sw/icons/numix-icon-theme/Numix /usr/share/icons/
 #sudo cp -rf $KALAN_DIR/sw/icons/numix-icon-theme/Numix-Light /usr/share/icons/
@@ -36,4 +59,3 @@ sudo cp -rf $KALAN_DIR/sw/icons/Faba /usr/share/icons/
 
 dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 > $KALAN_DIR-data/desktop-apt-get.fil
 #https://atom.io/
-
