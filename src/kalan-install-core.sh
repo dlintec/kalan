4a8537fdf0f624575a605978b6a81bfc2834389d
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-10 16:17:16 -0600

    Update kalan-install-core.sh

--- src/kalan-install-core.sh
+++ src/kalan-install-core.sh
@@ -2,7 +2,9 @@
 source ~/kalan/src/kalan-lib.sh
 KALAN_DIR="$HOME/kalan"
 PACKAGE_MANAGER=$(get_package_manager)
+current_dir=`pwd`
 cd $KALAN_DIR/sw/bashgui
 sudo make install
 ~/kalan/src/kalan-core-$PACKAGE_MANAGER.sh
 echo "DONE:kalan-install-core" >> ~/kalan-data/conf/flag_install
+cd $current_dir
