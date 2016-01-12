c51f4def18ca274a28ffe7eeac0e2a653879ac04
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-12 02:43:02 -0600

    Create alan-install-apps-01.sh

---
+++ src/alan-install-apps-01.sh
@@ -0,0 +1,3 @@
+#sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+#wget https://github.com/aptana/studio3/releases/download/v3.6.1/Aptana_Studio_3_Setup_Linux_x86_64_3.6.1.zip
+sudo apt-get -y install blender mypaint gimp scribus openshot avidemux  darktable inkscape virtualbox
