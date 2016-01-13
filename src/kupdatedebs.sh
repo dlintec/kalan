98aa54c1fdff9e7dc07b3a4e820e879a83acadaf
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-13 01:22:16 -0600

    added loop

--- src/kupdatedebs.sh
+++ src/kupdatedebs.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 KALAN_DIR="$HOME/kalan"
 
- if [ ! -d KALAN_DIR-data/downloads/debs ];then
-    mkdir -p KALAN_DIR-data/downloads/debs
- fi
- dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 > KALAN_DIR-data/result-all-apt-get.fil
- 
- cd KALAN_DIR-data/downloads/debs
- dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
- 
+if [ ! -d KALAN_DIR-data/downloads/debs ];then
+  mkdir -p KALAN_DIR-data/downloads/debs
+fi
+dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 > KALAN_DIR-data/result-all-apt-get.fil
+sudo rsync -aAXv sudo rsync -aAXv /var/cache/apt/archives/* $KALAN_DIR-data/downloads/debs
+localdeblist=$(ls $KALAN_DIR-data/downloads/debs)
+for itdeb in localdeblist ; do
+   echo $itdeb
+done
+cd KALAN_DIR-data/downloads/debs
+dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
+
