83d2bd310e638cc93d0fa01dcee26a66261233ac
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-13 11:12:24 -0600

    Create kalan-install-apps-ext.sh

---
+++ src/kalan-install-apps-ext.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+ 
+ 
+KALAN_USER="$(who am i | awk '{print $1}')" 
+KALAN_DIR="$HOME/kalan" 
+source $KALAN_DIR/src/kalan-lib.sh 
+PACKAGE_MANAGER=$(get_package_manager)  
+current_dir=`pwd`  
+
