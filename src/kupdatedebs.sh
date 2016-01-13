4eece6bde598291ba47500ce008263ebffd05ec0
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-13 14:59:26 -0600

    repodir="$KALAN_DIR-repo-deb"

--- src/kupdatedebs.sh
+++ src/kupdatedebs.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 KALAN_DIR="$HOME/kalan"
-
-if [ ! -d $KALAN_DIR-data/downloads/debs ];then
-  mkdir -p $KALAN_DIR-data/downloads/debs
+repodir="$KALAN_DIR-repo-deb"
+if [ ! -d $repodir ];then
+  mkdir -p $repodir
 fi
 dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 > KALAN_DIR-data/result-all-apt-get.fil
-rsync -aAXv sudo rsync -aAXv /var/cache/apt/archives/* $KALAN_DIR-data/downloads/debs
-localdeblist=$(ls $KALAN_DIR-data/downloads/debs)
+rsync -aAXv sudo rsync -aAXv /var/cache/apt/archives/* $repodir
+localdeblist=$(ls $repodir)
 for itdeb in localdeblist ; do
    echo $itdeb
 done
-cd $KALAN_DIR-data/downloads/debs
+cd $repodir
 dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
 
