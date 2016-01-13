6b860f7021f5b32e5b172e2d1c0fe1232bb057f1
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-13 01:23:23 -0600

    less sudo

--- src/kupdatedebs.sh
+++ src/kupdatedebs.sh
@@ -5,7 +5,7 @@ if [ ! -d KALAN_DIR-data/downloads/debs ];then
   mkdir -p KALAN_DIR-data/downloads/debs
 fi
 dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 > KALAN_DIR-data/result-all-apt-get.fil
-sudo rsync -aAXv sudo rsync -aAXv /var/cache/apt/archives/* $KALAN_DIR-data/downloads/debs
+rsync -aAXv sudo rsync -aAXv /var/cache/apt/archives/* $KALAN_DIR-data/downloads/debs
 localdeblist=$(ls $KALAN_DIR-data/downloads/debs)
 for itdeb in localdeblist ; do
    echo $itdeb
