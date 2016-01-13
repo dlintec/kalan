c30f952428cf8e1cbeb62114fdc4f064334c91b3
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-13 02:21:38 -0600

    fixed missing $

--- src/kupdatedebs.sh
+++ src/kupdatedebs.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 KALAN_DIR="$HOME/kalan"
 
-if [ ! -d KALAN_DIR-data/downloads/debs ];then
-  mkdir -p KALAN_DIR-data/downloads/debs
+if [ ! -d $KALAN_DIR-data/downloads/debs ];then
+  mkdir -p $KALAN_DIR-data/downloads/debs
 fi
 dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 > KALAN_DIR-data/result-all-apt-get.fil
 rsync -aAXv sudo rsync -aAXv /var/cache/apt/archives/* $KALAN_DIR-data/downloads/debs
@@ -10,6 +10,6 @@ localdeblist=$(ls $KALAN_DIR-data/downloads/debs)
 for itdeb in localdeblist ; do
    echo $itdeb
 done
-cd KALAN_DIR-data/downloads/debs
+cd $KALAN_DIR-data/downloads/debs
 dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
 
