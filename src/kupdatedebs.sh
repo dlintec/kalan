e174c7c0e7068922a7818fffa211ae206ba8f78a
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-13 15:04:40 -0600

    sudo rsync -aAXv /var/cache/apt/archives/* $repodi

--- src/kupdatedebs.sh
+++ src/kupdatedebs.sh
@@ -5,7 +5,7 @@ if [ ! -d $repodir ];then
   mkdir -p $repodir
 fi
 dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 > $KALAN_DIR-data/result-all-apt-get.fil
-rsync -aAXv sudo rsync -aAXv /var/cache/apt/archives/* $repodir
+sudo rsync -aAXv /var/cache/apt/archives/* $repodir
 localdeblist=$(ls $repodir)
 for itdeb in localdeblist ; do
    echo $itdeb
