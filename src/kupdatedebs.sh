3e7538db61baa21cc6d1ee5f06aeffd3a5534e9b
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-13 01:00:51 -0600

    result-all.fil

--- src/kupdatedebs.sh
+++ src/kupdatedebs.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 KALAN_DIR="$HOME/kalan"
+
  if [ ! -d KALAN_DIR-data/downloads/debs ];then
     mkdir -p KALAN_DIR-data/downloads/debs
  fi
+ dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 > KALAN_DIR-data/result-all-apt-get.fil
+ 
  cd KALAN_DIR-data/downloads/debs
  dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
  
