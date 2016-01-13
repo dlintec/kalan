2deecad7df9604935eb568abb98669d67e7c970e
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-13 01:40:46 -0600

    check for bin folder

--- src/kregisterscriptsfolder.sh
+++ src/kregisterscriptsfolder.sh
@@ -2,9 +2,12 @@
 main() {
 ###script ~/kalan/src/kregisterscriptsfolder.sh
 filelines=$(ls ~/kalan/src)
+if [ ! -d ~/bin ];then
+   mkdir ~/bin
+fi
 for line in $filelines ; do
     #echo "Creando link para script $line"
-	~/kalan/src/kregisterscript.sh ~/kalan/src/$line
+	~/kalan/src/kregisterscript.sh ~/bin/$line
 done
 
 }
