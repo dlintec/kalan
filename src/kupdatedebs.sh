25e031c1bfa9f2d658a570a0cc88c736ce838abd
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-13 15:14:18 -0600

    --exclude  "lock"

--- src/kupdatedebs.sh
+++ src/kupdatedebs.sh
@@ -5,7 +5,7 @@ if [ ! -d $repodir ];then
   mkdir -p $repodir
 fi
 dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 > $KALAN_DIR-data/result-all-apt-get.fil
-sudo rsync -aAXv /var/cache/apt/archives/* $repodir
+sudo rsync -aAXv --exclude 'lock' /var/cache/apt/archives/* $repodir
 localdeblist=$(ls $repodir)
 for itdeb in localdeblist ; do
    echo $itdeb
