fe52d7455982bcf88548758c0b888173ef35fef8
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-15 12:30:38 -0600

    quotes fun

--- src/kupdatedebs.sh
+++ src/kupdatedebs.sh
@@ -23,12 +23,12 @@ while read packagename; do
     let nfound=nfound+1
 
   else
-    findinrepo2="$(find $repodir -name $fullfilename*.deb)"
+    findinrepo2="$(find $repodir -name "$fullfilename*.deb")"
     echo "$fullfilename" >> $KALAN_DIR-data/missing-all-apt-get.fil
     echo "getting package: $packagename" 
     echo "in repo: $findinrepo2" 
     apt-get download $packagename
-    findinrepo3="$(find $repodir -name $fullfilename*.deb)"
+    findinrepo3="$(find $repodir -name "$fullfilename*.deb")"
     echo "after dl : $findinrepo3"
     let nmissing=nmissing+1
   fi
