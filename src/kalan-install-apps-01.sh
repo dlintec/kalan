1d457e90ac65a7fe582b5af24db785a3fa52ac8f
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-16 01:44:18 -0600

    Update kalan-install-apps-01.sh

--- src/kalan-install-apps-01.sh
+++ src/kalan-install-apps-01.sh
@@ -4,7 +4,6 @@ KALAN_USER="$(who am i | awk '{print $1}')"
 KALAN_DIR="$HOME/kalan" 
 current_dir=`pwd` 
 
-sudo apt-get -y install blender mypaint gimp scribus openshot avidemux  darktable inkscape vlc browser-plugin-vlc
 atom_ver="v1.3.3"
 if [[ ! -e $KALAN_DIR-data/downloads/atom-amd64.deb.$atom_ver ]];then
     cd $KALAN_DIR-data/downloads
@@ -17,6 +16,19 @@ if  [ -d $KALAN_DIR-data/downloads/atom/packages ];then
    mkdir -p $HOME/.atom/packages
    cp -rf $KALAN_DIR-data/downloads/atom/packages/* $HOME/.atom/packages
 fi
+chromelist="/etc/apt/sources.list.d/google-chrome.list"
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+if [[ -z $(grep "deb http://dl.google.com/linux/chrome/deb/ stable main" $chromelist) ]]; then  
+    sudo sh -c "echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' >> $chromelist"
+fi 
+sudo apt-get update
+sudo apt-get -y install google-chrome-unstable
+
+
+sudo apt-get -y install blender mypaint gimp scribus openshot avidemux  darktable inkscape vlc browser-plugin-vlc
+
 #sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
 #wget https://github.com/aptana/studio3/releases/download/v3.6.1/Aptana_Studio_3_Setup_Linux_x86_64_3.6.1.zip
 dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 > $KALAN_DIR-data/result-apps-01-apt-get.fil
