3c4f78e7d79634631bd8f469ecabb0cdbe5be84d
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-16 01:42:19 -0600

    Update kalan-install-apps-ext.sh

--- src/kalan-install-apps-ext.sh
+++ src/kalan-install-apps-ext.sh
@@ -7,15 +7,8 @@ source $KALAN_DIR/src/kalan-lib.sh
 PACKAGE_MANAGER=$(get_package_manager)  
 current_dir=`pwd`  
 
-chromelist="/etc/apt/sources.list.d/google-chrome.list"
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-
-if [[ -z $(grep "deb http://dl.google.com/linux/chrome/deb/ stable main" $chromelist) ]]; then  
-    sudo sh -c "echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' >> $chromelist"
-fi 
-sudo apt-get update
-sudo apt-get -y install google-chrome-unstable
-sudo apt-get -y install virtualbox  
+#sudo apt-get -y install virtualbox  
+kalan-install-docker
 
 
