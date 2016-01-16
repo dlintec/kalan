34f58c15ea36cbd18405c845597943e0fbfda02d
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-16 00:48:41 -0600

    Update kalan-install-docker.sh

--- src/kalan-install-docker.sh
+++ src/kalan-install-docker.sh
@@ -5,8 +5,8 @@ PACKAGE_MANAGER=$(get_package_manager)
 sudo curl -sSL https://get.docker.com/ | sh
 sudo $PACKAGE_MANAGER -y upgrade docker
 sudo service docker start
-sudo systemctl enable docker
-sudo service docker enable
+#sudo systemctl enable docker
+#sudo service docker enable
 sudo curl -L https://github.com/docker/machine/releases/download/v0.5.3/docker-machine_linux-amd64 > docker-machine
 sudo mv -f docker-machine /usr/local/bin/docker-machine
 sudo chmod 751 /usr/local/bin/docker-machine
