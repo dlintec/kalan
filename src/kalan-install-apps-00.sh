972b4b19b70673de7885c076627859653b9649d4
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-16 09:01:43 -0600

    Update kalan-install-apps-00.sh

--- src/kalan-install-apps-00.sh
+++ src/kalan-install-apps-00.sh
@@ -10,7 +10,17 @@ sudo add-apt-repository -y ppa:docky-core/stable
 sudo apt-get update
 sudo apt-get -y install plank
 
+sudo apt-get -y install --no-install-recommends gnome-panel
+sudo apt-get install g++ cmake qtbase5-dev libqt5x11extras5-dev libqt5svg5 libmuparser-dev
+
+git clone --recursive https://github.com/ManuelSchneid3r/albert.git $KALAN_DIR-data/downloads/albert
+cd $KALAN_DIR-data/downloads/albert
+cmake . -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release
+make
+sudo make install
+
 sudo apt-get -y install lupin-casper squashfs-tools
+
 function _install_systemback () {
       echo "removing"
       sudo apt-get -y purge systemback
@@ -21,29 +31,29 @@ function _install_systemback () {
       sudo apt-get -y purge systemback-scheduler
       sudo apt-get clean
       rm -rf $KALAN_DIR-data/build/sb
-    if [[ ! -e $KALAN_DIR-data/build/sb/systemback_1.7.301_amd64.deb ]];then
+      #if [[ ! -e $KALAN_DIR-data/build/sb/systemback_1.7.301_amd64.deb ]];then
       if [ ! -d $KALAN_DIR-data/build ];then
           mkdir -p $KALAN_DIR-data/build
       fi
       cp -rf $KALAN_DIR/sw/sb $KALAN_DIR-data/build/sb
       cd $KALAN_DIR-data/build/sb/systemback
       sudo debuild
-    fi
-    sudo cp -f $KALAN_DIR-data/build/sb/libsystemback_1.7.301_amd64.deb /var/cache/apt/archives
-    sudo cp -f $KALAN_DIR-data/build/sb/systemback-locales_1.7.301_all.deb /var/cache/apt/archives
-    sudo cp -f $KALAN_DIR-data/build/sb/systemback-cli_1.7.301_amd64.deb /var/cache/apt/archives
-    sudo cp -f $KALAN_DIR-data/build/sb/systemback-efiboot-amd64_1.7.301_all.deb /var/cache/apt/archives
-    sudo cp -f $KALAN_DIR-data/build/sb/systemback-scheduler_1.7.301_amd64.deb /var/cache/apt/archives
-    sudo cp -f $KALAN_DIR-data/build/sb/systemback_1.7.301_amd64.deb /var/cache/apt/archives
-    
-    sudo dpkg -i $KALAN_DIR-data/build/sb/libsystemback_1.7.301_amd64.deb
-    sudo dpkg -i $KALAN_DIR-data/build/sb/systemback-locales_1.7.301_all.deb
-    sudo dpkg -i $KALAN_DIR-data/build/sb/systemback-cli_1.7.301_amd64.deb
-    sudo dpkg -i $KALAN_DIR-data/build/sb/systemback-efiboot-amd64_1.7.301_all.deb
-    sudo dpkg -i $KALAN_DIR-data/build/sb/systemback-scheduler_1.7.301_amd64.deb
-    sudo dpkg -i $KALAN_DIR-data/build/sb/systemback_1.7.301_amd64.deb
-
-    sudo apt-get -f install
+      #fi
+      sudo cp -f $KALAN_DIR-data/build/sb/libsystemback_1.7.301_amd64.deb /var/cache/apt/archives
+      sudo cp -f $KALAN_DIR-data/build/sb/systemback-locales_1.7.301_all.deb /var/cache/apt/archives
+      sudo cp -f $KALAN_DIR-data/build/sb/systemback-cli_1.7.301_amd64.deb /var/cache/apt/archives
+      sudo cp -f $KALAN_DIR-data/build/sb/systemback-efiboot-amd64_1.7.301_all.deb /var/cache/apt/archives
+      sudo cp -f $KALAN_DIR-data/build/sb/systemback-scheduler_1.7.301_amd64.deb /var/cache/apt/archives
+      sudo cp -f $KALAN_DIR-data/build/sb/systemback_1.7.301_amd64.deb /var/cache/apt/archives
+      
+      sudo dpkg -i $KALAN_DIR-data/build/sb/libsystemback_1.7.301_amd64.deb
+      sudo dpkg -i $KALAN_DIR-data/build/sb/systemback-locales_1.7.301_all.deb
+      sudo dpkg -i $KALAN_DIR-data/build/sb/systemback-cli_1.7.301_amd64.deb
+      sudo dpkg -i $KALAN_DIR-data/build/sb/systemback-efiboot-amd64_1.7.301_all.deb
+      sudo dpkg -i $KALAN_DIR-data/build/sb/systemback-scheduler_1.7.301_amd64.deb
+      sudo dpkg -i $KALAN_DIR-data/build/sb/systemback_1.7.301_amd64.deb
+      
+      sudo apt-get -f install
 }
 
 if ! [ -x "$(command -v systemback-cli)" ]; then
@@ -54,14 +64,8 @@ if ! [ -x "$(command -v systemback-cli)" ]; then
 fi
 
 _install_systemback $param1
-sudo apt-get -y install --no-install-recommends gnome-panel
 
-sudo apt-get install g++ cmake qtbase5-dev libqt5x11extras5-dev libqt5svg5 libmuparser-dev
-git clone --recursive https://github.com/ManuelSchneid3r/albert.git $KALAN_DIR-data/downloads/albert
-cd $KALAN_DIR-data/downloads/albert
-cmake . -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release
-make
-sudo make install
+
 
 
 gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
