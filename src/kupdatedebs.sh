5f9f93c688d224d6199274f0817293d7f86d21f6
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-15 23:28:07 -0600

    Update kupdatedebs.sh

--- src/kupdatedebs.sh
+++ src/kupdatedebs.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 KALAN_DIR="$HOME/kalan"
-repodir="$KALAN_DIR-repo-deb"
+repodir="/var/kalan-repo-deb"
+source $KALAN_DIR/src/kalan-lib.sh
 if [ ! -d $repodir ];then
-  mkdir -p $repodir
+  sudo mkdir -p $repodir
 fi
 dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 | awk -F':' '{print $1}'> $KALAN_DIR-data/result-all-apt-get.fil
 sudo rsync -aAXv --exclude 'lock' /var/cache/apt/archives/* $repodir
@@ -33,9 +34,9 @@ while read packagename; do
     echo "getting package: $packagename version: $debversion " 
     echo "in repo: $findinrepo2" 
     if [[ $debversion == *['!'@#\$%^\&*()_+:]* ]];then
-       apt-get --no-install-recommends download $packagename
+       sudo apt-get --no-install-recommends download $packagename
     else
-       apt-get --no-install-recommends download $packagename=$debversion
+       sudo apt-get --no-install-recommends download $packagename=$debversion
     fi
     
       findinrepo3=$(find $repodir -name "${packagename}_*.deb")
@@ -54,3 +55,10 @@ dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
 echo "Total installed: $ntotal"
 echo "already in repo: $nfound"
 echo "missing        : $nmissing"
+
+localrepsource="deb file:$repodir ./"
+if [[ -z $(grep "$localrepsource" /etc/apt/sources.list) ]]; then 
+     echo "$localrepsource"  >> /etc/apt/sources.list
+     echo "Adding local repo"
+fi
+        
