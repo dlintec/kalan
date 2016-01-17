45e94712f0d953c895bd74bdec9b4384f83d98f7
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-17 15:15:32 -0600

    if [ $RESULT -eq 0 ]; then

--- src/kbuildimage.sh
+++ src/kbuildimage.sh
@@ -3,11 +3,18 @@
 main() {
    dockerfile="$1";shift;
    param2="$1";shift;
-   if [[ -d ~/kalan/dockerfiles/$dockerfile ]];then
-      cd ~/kalan/dockerfiles/$dockerfile
+   KALAN_DIR="$HOME/kalan"
+   if [[ $KALAN_DIR/dockerfiles/$dockerfile ]];then
+      cd $KALAN_DIR/dockerfiles/$dockerfile
       sudo docker build -t $dockerfile .
+      RESULT=$?
+      if [ $RESULT -eq 0 ]; then
+        echo success
+      else
+        echo failed
+      fi
    else
-      echo "Must provide a valid folder name in ~/kalan/dockerfiles with valid dockerfile"
+      echo "Must provide a valid folder name in $KALAN_DIR/dockerfiles with valid dockerfile"
       echo "folder:$dockerfile"
       echo "$param2"
    fi
