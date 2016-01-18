d9077655a74f043318599127e64b3268778800bf
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-18 15:54:58 -0600

    Update kalan-install-desktop.sh

--- src/kalan-install-desktop.sh
+++ src/kalan-install-desktop.sh
@@ -8,7 +8,7 @@ current_dir=`pwd`
 sudo cp -rf $KALAN_DIR/media/kalan-gray.* /usr/local/share/kalan
 gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/kalan/kalan-gray.jpg
 
-sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
+#sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
 if [[ -z $(grep -q "vm.swappiness" /etc/sysctl.conf) ]]; then
    echo "vm.swappiness=10"  >> /etc/sysctl.conf
    echo "Adding path"
