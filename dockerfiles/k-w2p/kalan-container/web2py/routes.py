23812fa06102421fd27b9147b9e0978f9b8ab666
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-17 20:32:01 -0600

    Delete routes.py

--- dockerfiles/k-w2p/kalan-container/web2py/routes.py
+++
@@ -1,6 +0,0 @@
-routers = dict(
-    BASE = dict(
-        default_application='kalan-start',
-        root_static = ['favicon.ico', 'robots.txt','sw-import.js','precache.json','manifest.json'],
-    )
-)
