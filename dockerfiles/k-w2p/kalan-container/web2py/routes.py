0d7763088fc4c3463ce37bc11f2e95a17ed9c69d
tadeogutierrez <tadeogutierrezsanchez@gmail.com>
2016-01-17 19:16:21 -0600

    added airstyle

---
+++ dockerfiles/k-w2p/kalan-container/web2py/routes.py
@@ -0,0 +1,6 @@
+routers = dict(
+    BASE = dict(
+        default_application='kalan-start',
+        root_static = ['favicon.ico', 'robots.txt','sw-import.js','precache.json','manifest.json'],
+    )
+)
