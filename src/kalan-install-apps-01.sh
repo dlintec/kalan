2105f44a4b3f43f289439d6a93f79eade384c5c1
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-18 11:20:10 -0600

    Update kalan-install-apps-01.sh

--- src/kalan-install-apps-01.sh
+++ src/kalan-install-apps-01.sh
@@ -31,7 +31,7 @@ sudo apt-get -y install google-chrome-unstable
 sudo rm -rf $chromelist
 
 
-sudo apt-get -y install blender mypaint gimp scribus openshot avidemux  darktable inkscape vlc browser-plugin-vlc
+sudo apt-get -y install blender mypaint gimp scribus openshot avidemux  darktable inkscape vlc browser-plugin-vlc audacity
 
 #sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
 #wget https://github.com/aptana/studio3/releases/download/v3.6.1/Aptana_Studio_3_Setup_Linux_x86_64_3.6.1.zip
