125a7da200e7ab18dc4c46fc274ec5e285ca342d
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-19 16:31:36 -0600

    Update kalan-personalize-lxde.sh

--- src/kalan-personalize-lxde.sh
+++ src/kalan-personalize-lxde.sh
@@ -5,6 +5,7 @@ sudo apt-get install conky-all
 sudo cp -rf $KALAN_DIR/media/kalan*.* /usr/local/share/kalan
 sudo cp -rf $KALAN_DIR/media/plymouth/*.* /lib/plymouth/themes/lubuntu-logo
 sudo chmod -R 755 /usr/local/share/kalan
+sudo update-initramfs -k all -u
 #gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/kalan/kalan-gray.jpg
 #sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
 
