6a877c85a36812a79c6bd7968db93abe7a5efc44
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-20 21:35:19 -0600

    chmod +x kalan-download.sh

--- kalan-download.sh
+++ kalan-download.sh
@@ -7,6 +7,7 @@
 main() {
 PARAMETRO="$1"
 wget https://raw.githubusercontent.com/dlintec/kalan/master/kalan-download.sh
+chmod +x kalan-download.sh
 clear
 echo "______________________________________________________________________________"
 echo " "
