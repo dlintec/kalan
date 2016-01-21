45c24aed634a6775ed4e8f9d9cc070a0d74673e5
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-20 21:30:35 -0600

    Update and rename kalan-install-apps-ext.sh to kalan-install-vm-tools.sh

--- src/kalan-install-vm-tools.sh
+++ src/kalan-install-vm-tools.sh
@@ -7,7 +7,7 @@ source $KALAN_DIR/src/kalan-lib.sh
 PACKAGE_MANAGER=$(get_package_manager)  
 current_dir=`pwd`  
 
-
+sudo apt-get -y install linux-headers-$(uname -r) gcc make
 #sudo apt-get -y install virtualbox  
 
 
