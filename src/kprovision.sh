81734eea2a3b7a07c77cd78cf6d659e667afb186
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-23 19:29:15 -0600

    Update kprovision.sh

--- src/kprovision.sh
+++ src/kprovision.sh
@@ -97,20 +97,20 @@ for arg in "$@" ; do
 			--name $provisionname-config \
 			$image_name \
 			/var/kalan-container/web2py/web2py.py --nogui -i 0.0.0.0 -p 8888 -a "<recycle>"
-			sudo docker exec $provisionname-config cp -a $container_appfolder-backup/. /var/kalan-container/web2py/applications/
-			sudo docker exec $provisionname-config chown -R 999:999 /var/kalan-container
+			#sudo docker exec $provisionname-config cp -a $container_appfolder-backup/. /var/kalan-container/web2py/applications/
+			#sudo docker exec $provisionname-config chown -R 999:999 /var/kalan-container
 			
 			if [[ -n "$adminauth" ]];then
-				certCN="localhost.localdomain"
-				sudo docker exec $provisionname-config mkdir -p $container_sslfolder
-				sudo docker exec $provisionname-config openssl genrsa -des3 -passout pass:x -out $container_sslfolder/certif.pass.key 2048
-				sudo docker exec $provisionname-config openssl rsa -passin pass:x -in $container_sslfolder/certif.pass.key -out $container_sslfolder/self_signed.key
-				sudo docker exec $provisionname-config rm $container_sslfolder/certif.pass.key
-				sudo docker exec $provisionname-config openssl req -new -key $container_sslfolder/self_signed.key -out $container_sslfolder/self_signed.csr -subj "/C=MX/ST=Mexico/L=DF/O=seguraxes/OU=dlintec/CN=$certCN"
-				sudo docker exec $provisionname-config openssl x509 -req -days 1000 -in $container_sslfolder/self_signed.csr -signkey  $container_sslfolder/self_signed.key -out $container_sslfolder/self_signed.cert
-				sudo docker exec $provisionname-config chmod -R 550 $container_sslfolder
-				sudo docker exec $provisionname-config chgrp -R 999 $container_sslfolder
-				sudo docker exec $provisionname-config chown -R kcontainer:kcontainer $container_sslfolder
+				#certCN="localhost.localdomain"
+				#sudo docker exec $provisionname-config mkdir -p $container_sslfolder
+				#sudo docker exec $provisionname-config openssl genrsa -des3 -passout pass:x -out $container_sslfolder/certif.pass.key 2048
+				#sudo docker exec $provisionname-config openssl rsa -passin pass:x -in $container_sslfolder/certif.pass.key -out $container_sslfolder/self_signed.key
+				#sudo docker exec $provisionname-config rm $container_sslfolder/certif.pass.key
+				#sudo docker exec $provisionname-config openssl req -new -key $container_sslfolder/self_signed.key -out $container_sslfolder/self_signed.csr -subj "/C=MX/ST=Mexico/L=DF/O=seguraxes/OU=dlintec/CN=$certCN"
+				#sudo docker exec $provisionname-config openssl x509 -req -days 1000 -in $container_sslfolder/self_signed.csr -signkey  $container_sslfolder/self_signed.key -out $container_sslfolder/self_signed.cert
+				#sudo docker exec $provisionname-config chmod -R 550 $container_sslfolder
+				#sudo docker exec $provisionname-config chgrp -R 999 $container_sslfolder
+				#sudo docker exec $provisionname-config chown -R kcontainer:kcontainer $container_sslfolder
 				
 				#sudo docker rm $provisionname-config
 			fi
