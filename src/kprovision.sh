f3d1e2b6562511e00a5759300a05d9340fded4c4
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-23 21:24:00 -0600

    Update kprovision.sh

--- src/kprovision.sh
+++ src/kprovision.sh
@@ -3,8 +3,15 @@
 main() {
 provisionname="$1";shift
 rebuild="false"
+runprovision="false"
 for arg in "$@" ; do
        case "$arg" in
+         run)
+           provisionname=$2
+           runprovision="true"
+           shift
+           ;;
+
          -n)
            provisionname=$2
            shift
@@ -30,38 +37,38 @@ for arg in "$@" ; do
            shift
            ;;
         esac
-   done
-  echo "name  = $provisionname"
-  echo "image = $image_name"
-  echo "apps  = $src_w2papps"
-  KALAN_USER="$(who am i | awk '{print $1}')"
-  KALAN_DIR="$HOME/kalan"
+done
+echo "name  = $provisionname"
+echo "image = $image_name"
+echo "apps  = $src_w2papps"
+KALAN_USER="$(who am i | awk '{print $1}')"
+KALAN_DIR="$HOME/kalan"
 
-   KALAN_PROVISIONS_DIR="$KALAN_DIR-data/provisions"
-   provisioncreated=false;
-   container_appfolder="/var/kalan-container/web2py/applications"
-   container_sslfolder="/var/kalan-container/ssl"
-   ssl_folder="/var/kalan-container/ssl"
-   if [[ -z "$image_name" ]];then
-	 image_name="k-w2p"
-   fi
+KALAN_PROVISIONS_DIR="$KALAN_DIR-data/provisions"
+provisioncreated="false";
+container_appfolder="/var/kalan-container/web2py/applications"
+container_sslfolder="/var/kalan-container/ssl"
+ssl_folder="/var/kalan-container/ssl"
+if [[ -z "$image_name" ]];then
+	image_name="k-w2p"
+fi
+
    
-
-   if [[ ! -d $KALAN_PROVISIONS_DIR/$provisionname ]];then
-	   if [[ "$rebuild" == "true" ]];then
-	   	echo "rebuilding..."
-	           #$KALAN_DIR/src/kprovision.sh $provisionname --remove
-	           if sudo docker history -q $image_name 2>&1 >/dev/null; then
-		    	echo "Rebuilding: $image_name"
-		    	if [[ -e $KALAN_DIR-data/docker-images/$image_name.tar ]];then
-		    		now=$(date +"%Y.%m.%d.%S.%N")
-				filename="$KALAN_DIR-data/docker-images/$image_name-$now.tar"
-				mv $KALAN_DIR-data/docker-images/$image_name.tar $filename
-			fi
-		    	sudo docker rmi $image_name
-		   fi
-		   
+if [[ ! -d $KALAN_PROVISIONS_DIR/$provisionname ]];then
+	if [[ "$rebuild" == "true" ]];then
+	   echo "rebuilding..."
+	   #$KALAN_DIR/src/kprovision.sh $provisionname --remove
+	   if sudo docker history -q $image_name 2>&1 >/dev/null; then
+	    	echo "Rebuilding: $image_name"
+	    	if [[ -e $KALAN_DIR-data/docker-images/$image_name.tar ]];then
+	    		now=$(date +"%Y.%m.%d.%S.%N")
+			filename="$KALAN_DIR-data/docker-images/$image_name-$now.tar"
+			mv $KALAN_DIR-data/docker-images/$image_name.tar $filename
+		fi
+	    	sudo docker rmi $image_name
 	   fi
+	   
+	fi
 
 	if sudo docker history -q $image_name 2>&1 >/dev/null; then
 	    	echo "image Ok: $image_name exists in docker cache"
@@ -119,49 +126,54 @@ for arg in "$@" ; do
 			#fi
 			echo "stoping config container"
 			sudo docker stop $provisionname-config
+			provisioncreated="true";
 
-			echo "Starting container"
-
-			sudo docker run -p 8443:8443 -p 8888:8888 -d\
-				--volumes-from $provisionname-provision \
-				--entrypoint /usr/bin/python \
-				--name $provisionname \
-				$image_name \
-				/var/kalan-container/web2py/web2py.py --nogui -i 0.0.0.0 -p 8888 -a "<recycle>"
-
-			if [[ -n "$adminauth" ]];then
-				echo "Starting admin interface"
-				sudo docker exec -d $provisionname python /var/kalan-container/web2py/web2py.py --nogui -i 0.0.0.0 -p 8443 -a "$adminauth" -k $container_sslfolder/self_signed.key -c $container_sslfolder/self_signed.cert
-			fi
 		else
 			echo "Failed creating new provision for data container: $provisionname-provision"
 		fi
 	
-		provisioncreated=true;
+		
 	else
 	       echo "Failed creating new provision. Image $image_name is not in cache"
 	fi
 
 
-   else
-      if [[ "$src_w2papps" == "--remove" ]];then
-         echo "removing provision $provisionname"
-         sudo docker stop $provisionname
-         sudo docker rm -v $provisionname
-         sudo docker rm -v $provisionname-provision
-         sudo docker rm -v $provisionname-config
-         if [ -d $KALAN_PROVISIONS_DIR/$provisionname ];then
-            sudo rm -rf $KALAN_PROVISIONS_DIR/$provisionname
-         else
-            echo "error removing provision $KALAN_PROVISIONS_DIR/$provisionname"
-         fi
-      else
-         echo "There is previous prevision with name $provisionname"
-         echo "folder: $KALAN_PROVISIONS_DIR/$provisionname"
-         ls $KALAN_PROVISIONS_DIR
-         echo "apps folder: $src_w2papps"
-      fi
-   fi
+else
+	if [[ "$src_w2papps" == "--remove" ]];then
+		 echo "removing provision $provisionname"
+		 sudo docker stop $provisionname
+		 sudo docker rm -v $provisionname
+		 sudo docker rm -v $provisionname-provision
+		 sudo docker rm -v $provisionname-config
+		 if [ -d $KALAN_PROVISIONS_DIR/$provisionname ];then
+		    sudo rm -rf $KALAN_PROVISIONS_DIR/$provisionname
+		 else
+		    echo "error removing provision $KALAN_PROVISIONS_DIR/$provisionname"
+		 fi
+	else
+	         provisioncreated="true";
+	         echo "There is previous prevision with name $provisionname"
+	         echo "folder: $KALAN_PROVISIONS_DIR/$provisionname"
+	         #ls $KALAN_PROVISIONS_DIR
+	         echo "apps folder: $src_w2papps"
+		echo "Starting container"
+
+      	fi
+fi
+
+if [[ "$provisioncreated"=="true" ]];then
+	sudo docker run -p 8443:8443 -p 8888:8888 -d\
+		--volumes-from $provisionname-provision \
+		--entrypoint /usr/bin/python \
+		--name $provisionname \
+		$image_name \
+		/var/kalan-container/web2py/web2py.py --nogui -i 0.0.0.0 -p 8888 -a "<recycle>"
+	
+	if [[ -n "$adminauth" ]];then
+		echo "Starting admin interface"
+		sudo docker exec -d $provisionname python /var/kalan-container/web2py/web2py.py --nogui -i 0.0.0.0 -p 8443 -a "$adminauth" -k $container_sslfolder/self_signed.key -c $container_sslfolder/self_signed.cert
+	fi
+fi
 }
 
 main "$@"
