f767e5c3e66ba944b24ec7e7ed29c96cc3223631
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-25 17:38:03 -0600

    Update kalan-update.sh

--- src/kalan-update.sh
+++ src/kalan-update.sh
@@ -12,42 +12,73 @@ current_dir=`pwd`
 
 source $KALAN_DIR/src/kalan-lib.sh
 
-cd /var/
-if [ ! -e $KALAN_DIR/README.md ];then
-   git clone --recursive https://github.com/dlintec/kalan.git $KALAN_DIR
+current_dir=`pwd`
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/gentoo-release]=emerge
+osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/debian_version]=apt-get
+PACKAGE_MANAGER="yum"
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        PACKAGE_MANAGER=${osInfo[$f]}
+        echo $PACKAGE_MANAGER
+    fi
+done 
+#$PACKAGE_MANAGER -y install git curl
+if ! [ -x "$(command -v git)" ]; then
+  echo "-------------------------------------------------------------------------"
+  echo "   Git is not installed"
+  echo "   We will use: sudo $PACKAGE_MANAGER install git"
+  echo "-------------------------------------------------------------------------"
+ 
+  sudo apt-get -y install git
+  echo "   Git installed: "
+  echo "   curl -L https://raw.githubusercontent.com/dlintec/kalan/master/kalan-download.sh | bash -i"
+  echo
 fi
-cd $KALAN_DIR
-git fetch --all
-git reset --hard origin/master
-git pull
-chmod -R 770 $KALAN_DIR/src
-chmod +x $KALAN_DIR/src/kalan.sh
-chmod +x $KALAN_DIR/src/kregisterscript.sh
-chmod +x $KALAN_DIR/src/kregisterscriptsfolder.sh
-chmod -R 775 $KALAN_DIR/media
+if [ -n "$(command -v git)" ]; then
+   cd /var/
+   if [ ! -e $KALAN_DIR/README.md ];then
+      git clone --recursive https://github.com/dlintec/kalan.git $KALAN_DIR
+   fi
+   cd $KALAN_DIR
+   git fetch --all
+   git reset --hard origin/master
+   git pull
+   chmod -R 770 $KALAN_DIR/src
+   chmod +x $KALAN_DIR/src/kalan.sh
+   chmod +x $KALAN_DIR/src/kregisterscript.sh
+   chmod +x $KALAN_DIR/src/kregisterscriptsfolder.sh
+   chmod -R 775 $KALAN_DIR/media
+   
+   
+   $KALAN_DIR/src/kregisterscriptsfolder.sh
+   if [[ ! -d /usr/local/share/kalan ]];then
+     sudo mkdir -p /usr/local/share/kalan
+   fi
+   if [[ ! -d ~/bin ]];then
+     mkdir -p ~/bin
+   fi
+   sudo cp -rf $KALAN_DIR/media/kalan*.* /usr/local/share/kalan
+   
+   sudo cp -a $KALAN_DIR/sw/lubuntu/wallpapers/*.* /usr/share/backgrounds
+   sudo chmod -R 755 /usr/share/backgrounds
+   sudo chmod -R 755 $KALAN_DIR/src/kalan-update.sh
+   sudo chown -R root:root /usr/share/backgrounds
+   sudo cp -a $KALAN_DIR/sw/*.desktop /usr/share/applications/
+   sudo chmod -R 755 /usr/share/applications/kalan-update.desktop
+   sudo cp -a /usr/share/applications/kalan-update.desktop $HOME/Desktop/
+   sudo chmod -R 755 $HOME/Desktop/kalan-update.desktop
+   sudo cp -a $KALAN_DIR/src/kalan-update.sh /usr/local/bin/
+   gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/kalan-blue.jpg
 
-
-$KALAN_DIR/src/kregisterscriptsfolder.sh
-if [[ ! -d /usr/local/share/kalan ]];then
-  sudo mkdir -p /usr/local/share/kalan
+else
+   echo
+   echo "  You need to install 'git' to download kalan"
 fi
-if [[ ! -d ~/bin ]];then
-  mkdir -p ~/bin
-fi
-sudo cp -rf $KALAN_DIR/media/kalan*.* /usr/local/share/kalan
-
-sudo cp -a $KALAN_DIR/sw/lubuntu/wallpapers/*.* /usr/share/backgrounds
-sudo chmod -R 755 /usr/share/backgrounds
-sudo chmod -R 755 $KALAN_DIR/src/kalan-update.sh
-sudo chown -R root:root /usr/share/backgrounds
-sudo cp -a $KALAN_DIR/sw/*.desktop /usr/share/applications/
-sudo chmod -R 755 /usr/share/applications/kalan-update.desktop
-sudo cp -a /usr/share/applications/kalan-update.desktop $HOME/Desktop/
-sudo chmod -R 755 $HOME/Desktop/kalan-update.desktop
-sudo cp -a $KALAN_DIR/src/kalan-update.sh /usr/local/bin/
-gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/kalan-blue.jpg
-
-
 
 }
 
