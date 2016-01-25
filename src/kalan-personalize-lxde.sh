ac7a88884f5fd4f1aac2c7de2af83d9ef1d73186
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-25 11:52:36 -0600

    Update kalan-personalize-lxde.sh

--- src/kalan-personalize-lxde.sh
+++ src/kalan-personalize-lxde.sh
@@ -19,9 +19,10 @@ rm -rf $HOME/.config/autostart/*.desktop
 cp -rf $KALAN_DIR/sw/autostart/*.desktop $HOME/.config/autostart/
 sudo cp -rf  $KALAN_DIR/sw/lubuntu-logout.desktop /usr/share/applications/
 sudo chown -R root:root /usr/share/applications/lubuntu-logout.desktop 
-sudo chmod -R 755 /usr/share/applications/lubuntu-logout.desktop
+sudo chmod -R 644 /usr/share/applications/lubuntu-logout.desktop
 #gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/kalan/kalan-gray.jpg
 #sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
 #In pcmanfm preferences, under the advanced tab, change the terminal to "x-terminal-emulator -e %s". You can also change the "terminal" value in ~/.config/libfm/libfm.conf.
+sudo update-desktop-database
 
 sudo cp -f $KALAN_DIR/sw/lxde/lightdm-gtk-greeter-conf /etc/lightdm/lightdm-gtk-greeter.conf
