345aa4ce328b5491aae4d506daeb3f25587308e8
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-26 16:20:12 -0600

    Update docker-entrypoint.sh

--- dockerfiles/k-w2p/docker-entrypoint.sh
+++ dockerfiles/k-w2p/docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-if [[  ( "$1" = 'init')  ]]; then
-  if [[ ( ! -d /var/kalan-container/web2py/applications/admin ) ]];then
-    cp -a /var/kalan-container/web2py/applications-backup/. /var/kalan-container/web2py/applications/
-  fi
-  chown -R kcontainer:kcontainer /var/kalan-container/web2py/applications/
-fi
+if [[  ( "$1" = 'init') || ( ! -d /var/kalan-container/web2py/applications/admin )  ]]; then
 
+  cp -a /var/kalan-container/web2py/applications-backup/. /var/kalan-container/web2py/applications/
+
+
+fi
+chown -R kcontainer:kcontainer /var/kalan-container/web2py/applications/
 exec /usr/bin/python /var/kalan-container/web2py/web2py.py --nogui -i 0.0.0.0 -p 8888 -a "<recycle>"
 
 #exec "$@"
