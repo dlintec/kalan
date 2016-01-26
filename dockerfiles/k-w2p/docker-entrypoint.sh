abed88376580c55b68d2e109c1d89c9bbe16a950
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-26 15:21:22 -0600

    Create docker-entrypoint.sh

---
+++ dockerfiles/k-w2p/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+if [[  ( "$1" = 'init')  ]]; then
+  if [[ ( ! -d /var/kalan-container/web2py/applications/admin ) ]];then
+    cp -a /var/kalan-container/web2py/applications-backup/. /var/kalan-container/web2py/applications/
+  fi
+  chown -R kcontainer:kcontainer /var/kalan-container/web2py/applications/
+  /usr/bin/python /var/kalan-container/web2py/web2py.py --nogui -i 0.0.0.0 -p 8888 -a "<recycle>"
+fi
+
+
+exec "$@"
