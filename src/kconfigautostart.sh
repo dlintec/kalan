13c2a24562326593c6eb5c1f86f38f4c328bf5d8
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-26 18:06:31 -0600

    Update kconfigautostart.sh

--- src/kconfigautostart.sh
+++ src/kconfigautostart.sh
@@ -3,7 +3,7 @@ KALAN_DIR="$HOME/kalan"
 cat << EOFKALANSCRIPT > $HOME/.config/autostart/kalan-autostart.desktop
 [Desktop Entry]
 Type=Application
-Exec="$KALAN_DIR/src/kprovision.sh kalan --admin admin"
+Exec="$KALAN_DIR/src/kalan-autostart.sh"
 Hidden=false
 Terminal=true
 Name=kalan-autostart
