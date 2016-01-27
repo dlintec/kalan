d88c823b71483623d1a5a347bf0ea7561d0baa25
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-27 11:18:05 -0600

    Update kalan-autostart.sh

--- src/kalan-autostart.sh
+++ src/kalan-autostart.sh
@@ -9,4 +9,6 @@ kprovision kalan --remove
 clear
 kecho "Welcome $KALAN_USER"
 kprovision kalan --admin admin
-exec x-www-browser https://localhost:8443
+x-www-browser https://localhost:8443
+clear
+kecho
