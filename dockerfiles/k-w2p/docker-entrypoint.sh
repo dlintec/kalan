1e153a19df4caad64452db588af109fffa4a86a8
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-26 19:06:38 -0600

    Update docker-entrypoint.sh

--- dockerfiles/k-w2p/docker-entrypoint.sh
+++ dockerfiles/k-w2p/docker-entrypoint.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
+par1="$1"
+par2="$2"
+par3="$3"
+container_sslfolder="/var/kalan-container/ssl"
 set -e
 if [[ ( ! -d /var/kalan-container/web2py/applications/admin ) ]];then
   cp -a /var/kalan-container/web2py/applications-backup/. /var/kalan-container/web2py/applications/
 fi
 
-if [[  ( "$1" = 'init')  ]]; then
-  
+if [[  ( "$par1" = 'init')  ]]; then
+  echo "Starting kalan normal mode"
   chown -R kcontainer:kcontainer /var/kalan-container/web2py/applications/
   exec /usr/bin/python /var/kalan-container/web2py/web2py.py --nogui -i 0.0.0.0 -p 8888 -a "<recycle>"
+
+fi
+if [[  ( "$par1" = 'initadmin') && ( -n "$par2" ) ]]; then
+  echo "Starting kalan ADMIN MODE"
+  chown -R kcontainer:kcontainer /var/kalan-container/web2py/applications/
+  exec /usr/bin/python /var/kalan-container/web2py/web2py.py --nogui -i 0.0.0.0 -p 8443 -a "$par2" -k $container_sslfolder/self_signed.key -c $container_sslfolder/self_signed.cert
 
 fi
 
