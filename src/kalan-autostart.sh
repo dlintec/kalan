58d775f659776e0a2e2491668dc63415932e57b1
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-27 11:26:19 -0600

    Update kalan-autostart.sh

--- src/kalan-autostart.sh
+++ src/kalan-autostart.sh
@@ -2,13 +2,15 @@
 KALAN_USER="$(who am i | awk '{print $1}')"
 KALAN_DIR="$HOME/kalan"
 source ~/kalan/src/kalan-lib.sh
-export supermode="dialog" && source easybashgui
+#export supermode="dialog" && source easybashgui
+source easybashgui
 kecho "Welcome $KALAN_USER"
 sudo echo "  we need your superuser powers to start services"
 kprovision kalan --remove
 clear
 kecho "Welcome $KALAN_USER"
 kprovision kalan --admin admin
+
 wait_seconds 10
 exec x-www-browser https://localhost:8443
 clear
