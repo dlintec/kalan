0173a71229cc08531c39ae3ab8deb6077760f847
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-27 10:46:13 -0600

    Update kalan-autostart.sh

--- src/kalan-autostart.sh
+++ src/kalan-autostart.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+KALAN_USER="$(who am i | awk '{print $1}')"
 KALAN_DIR="$HOME/kalan"
+source ~/kalan/src/kalan-lib.sh
+export supermode="dialog" && source easybashgui
+
 kprovision kalan --remove
 clear
+kecho "Welcome $KALAN_USER"
 kprovision kalan --admin admin
