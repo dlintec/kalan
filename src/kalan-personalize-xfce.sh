ca6de8d96130c30d354a687211935d9cf8122e1b
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-27 20:51:05 -0600

    Update kalan-personalize-xfce.sh

--- src/kalan-personalize-xfce.sh
+++ src/kalan-personalize-xfce.sh
@@ -5,8 +5,8 @@ sudo cp -rf $KALAN_DIR/media/kalan*.* /usr/local/share/kalan
 
 sudo cp -a -L $KALAN_DIR/media/wallpapers/. $backdropfolder/
 if [[ ! -e $backdropfolder/xfce-blue-ant.jpg ]];then
-  sudo mv -f /usr/share/backgrounds/xfce/xfce-blue.jpg $backdropfolder/xfce-blue-ant.jpg
-  sudo ln -sf /usr/share/backgrounds/xfce/kalan-blue.jpg $backdropfolder/kalan-blue.jpg
+  sudo cp -f /usr/share/backgrounds/xfce/xfce-blue.jpg $backdropfolder/xfce-blue-ant.jpg
+  #sudo ln -sf /usr/share/backgrounds/xfce/kalan-blue.jpg $backdropfolder/kalan-blue.jpg
 fi
 sudo chmod -R 755 $backdropfolder
 
