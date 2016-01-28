669c2f08aa20cc2d5e1dd80491c9a3041c2222ad
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-27 22:09:03 -0600

    Update kalan-autostart.sh

--- src/kalan-autostart.sh
+++ src/kalan-autostart.sh
@@ -6,18 +6,35 @@ source ~/kalan/src/kalan-lib.sh
 source easybashgui
 clear
 kecho "Welcome $KALAN_USER"
-sudo echo "  we need your superuser powers to start services"
-kprovision kalan --remove
-clear
-kecho "Welcome $KALAN_USER"
-kprovision kalan --admin admin
-
-#wait_seconds 10
-#exec x-www-browser https://localhost:8443
-clear
-kecho "Connect with your browser to https://localhost:8443"
-echo "  This system also has the following IPs:"
-echo ""
-get-ip-address
-echo "Press enter to close this window"
-read CONFIRM
+if [[ -e $KALAN_DIR-data/conf/updates ]];then
+  sudo echo "  we need your superuser powers to start services"
+  kprovision kalan --remove
+  clear
+  kecho "Welcome $KALAN_USER"
+  kprovision kalan --admin admin
+  
+  #wait_seconds 10
+  #exec x-www-browser https://localhost:8443
+  clear
+  kecho "Connect with your browser to https://localhost:8443"
+  echo "  This system also has the following IPs:"
+  echo ""
+  get-ip-address
+  echo "Press enter to close this window"
+  read CONFIRM
+else
+  op_install="Install/Clone kalan in new system"
+  op_start="Start using kalan right now!"
+  menu  "$op_start" "$op_install"
+  installoption="$(cat ${dir_tmp}/${file_tmp})"  
+  rm -f ${dir_tmp}/${file_tmp}
+      case "$installoption" in
+         $op_install)
+           exec gksu gparted
+           exec gksu systemback
+           ;;
+         $op_start)
+           echo ""
+           ;;
+      esac
+fi
