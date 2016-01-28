08c085dbfddd75923c82882fe47aa417d98ea7e3
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-27 21:31:39 -0600

    Update kcleanforclone.sh

--- src/kcleanforclone.sh
+++ src/kcleanforclone.sh
@@ -4,4 +4,5 @@ kprovision kalan --remove
 kalan-clean-docker
 rm -rf $KALAN_DIR-data/downloads/*
 rm -rf $KALAN_DIR-data/build/*
+rm -f $KALAN_DIR-data/conf/updates
 echo "environment cleaned for clone"
