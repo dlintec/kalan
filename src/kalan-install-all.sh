1f1047075987d9d4107f6dde357ba32e1ee881d8
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-28 03:34:18 -0600

    Update kalan-install-all.sh

--- src/kalan-install-all.sh
+++ src/kalan-install-all.sh
@@ -6,6 +6,6 @@ $KALAN_DIR/src/kalan-install-desktop.sh
 $KALAN_DIR/src/kalan-install-apps-00.sh
 $KALAN_DIR/src/kalan-install-apps-01.sh
 $KALAN_DIR/src/kalan-install-touch.sh
-
+$KALAN_DIR/src/kalan-personalize-xfce.sh
 rm -f ~/kalan-install-all
 
