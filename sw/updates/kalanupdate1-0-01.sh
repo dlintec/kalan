f4fd9d28a77617990bb6e63411b26bd63b24fa75
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-29 16:47:19 -0600

    Update kalanupdate1-0-01.sh

--- sw/updates/kalanupdate1-0-01.sh
+++ sw/updates/kalanupdate1-0-01.sh
@@ -4,3 +4,15 @@ KALAN_DIR="$HOME/kalan"
 source ~/kalan/src/kalan-lib.sh
 #export supermode="dialog" && source easybashgui
 source easybashgui
+	        # for migration purposes of legacy apps
+		if [[ ( -d $KALAN_DIR-data/provisions/kalan/kalan-container/web2py ) ]];then
+		echo "Rebuilding and migrating from update"
+		  provision_image_folder=$KALAN_DIR-data/provisions/kalan/kalan-container/k-w2p
+		  mkdir -p $provision_image_folder/applications
+		  sudo cp -a $KALAN_DIR-data/provisions/kalan/kalan-container/web2py/applications/. $provision_image_folder/applications/
+		  sudo chown -R 999:999 $provision_image_folder/applications
+		  rm -rf $KALAN_DIR-data/provisions/kalan/kalan-container/web2py
+		fi
+rm -f $KALAN_DIR-data/docker-images/k-w2p.tar
+
+$KALAN_DIR/src/kbuildimage.sh $image_name
