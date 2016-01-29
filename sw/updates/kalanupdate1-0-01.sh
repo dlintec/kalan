a9511ea4886e0ab608dce2fac904f31965a2be3b
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-01-29 17:52:01 -0600

    Update kalanupdate1-0-01.sh

--- sw/updates/kalanupdate1-0-01.sh
+++ sw/updates/kalanupdate1-0-01.sh
@@ -19,6 +19,7 @@ echo "Rebuilding and migrating from update"
 fi
 rm -f $KALAN_DIR-data/docker-images/k-w2p.tar
 ##
-$KALAN_DIR/src/kbuildimage.sh $image_name
+
+$KALAN_DIR/src/kbuildimage.sh k-w2p
 
 $KALAN_DIR/src/kalan-autostart.sh
