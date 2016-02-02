a16d2cc35bc8fc7bf53b2f8d9e2f567434940649
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-02 02:23:34 -0600

    Update kalanupdate1-0-02.sh

--- sw/updates/kalanupdate1-0-02.sh
+++ sw/updates/kalanupdate1-0-02.sh
@@ -2,10 +2,7 @@
 KALAN_USER="$(who am i | awk '{print $1}')"
 KALAN_DIR="$HOME/kalan"
 source ~/kalan/src/kalan-lib.sh
-sudo curl -L https://github.com/docker/compose/releases/download/1.6.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-sudo chmod 751 /usr/local/bin/docker-compose
-sudo curl -L https://github.com/docker/machine/releases/download/v0.6.0-rc2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \
-sudo chmod 751 /usr/local/bin/docker-machine
+kalan-install-docker
 if [[ -z $(grep " --iptables=false" /etc/default/docker) ]]; then 
    echo "adding to /etc/default/docker"
   dkropt='"--dns 8.8.8.8 --dns 8.8.4.4 --iptables=false"'
