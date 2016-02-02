b6ed79551b3a26eecfb360b1f33e9489f8ed655d
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-02 00:01:03 -0600

    Update kalanupdate1-0-02.sh

--- sw/updates/kalanupdate1-0-02.sh
+++ sw/updates/kalanupdate1-0-02.sh
@@ -3,3 +3,8 @@ KALAN_USER="$(who am i | awk '{print $1}')"
 KALAN_DIR="$HOME/kalan"
 curl -L https://github.com/docker/compose/releases/download/1.6.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+echo 'DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4 --iptables=false" ' >> /etc/default/docker
+service restart docker
+
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
