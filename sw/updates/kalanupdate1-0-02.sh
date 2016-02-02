cc9907d602564950ed2829a4a19d12498c41516c
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-02 01:01:26 -0600

    Update kalanupdate1-0-02.sh

--- sw/updates/kalanupdate1-0-02.sh
+++ sw/updates/kalanupdate1-0-02.sh
@@ -4,16 +4,17 @@ KALAN_DIR="$HOME/kalan"
 sudo curl -L https://github.com/docker/compose/releases/download/1.6.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 if [[ -z $(grep "--iptables=false" /etc/default/docker) ]]; then 
-  sudo echo 'DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4 --iptables=false" ' >> /etc/default/docker
+  dkropt='"--dns 8.8.8.8 --dns 8.8.4.4 --iptables=false"'
+  sudo sh -c "echo 'DOCKER_OPTS= $dkropt' >> /etc/default/docker"
 fi
 sudo service restart docker
 
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 if [[ -z $(grep " net/ipv4/ip_forward=1 " /etc/ufw/sysctl.conf) ]]; then 
-  sudo echo " net/ipv4/ip_forward=1 " >> /etc/ufw/sysctl.conf
-  sudo echo "net/ipv6/conf/default/forwarding=1" >> /etc/ufw/sysctl.conf
-  sudo echo "net/ipv6/conf/all/forwarding=1" >> /etc/ufw/sysctl.conf
+  sudo sh -c "echo ' net/ipv4/ip_forward=1 ' >> /etc/ufw/sysctl.conf"
+  sudo sh -c "echo 'net/ipv6/conf/default/forwarding=1' >> /etc/ufw/sysctl.conf"
+  sudo sh -c "echo 'net/ipv6/conf/all/forwarding=1' >> /etc/ufw/sysctl.conf"
 fi 
 sudo ufw disable
 sudo ufw enable
