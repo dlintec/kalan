bbcf56e0f0593b7ec9545036b81fc1524f6f6a93
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-03 09:53:19 -0600

    Update kalan-install-docker.sh

--- src/kalan-install-docker.sh
+++ src/kalan-install-docker.sh
@@ -10,7 +10,7 @@ sudo service docker start
 #sudo systemctl enable docker
 #sudo service docker enable
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.6.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/1.6.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 sudo chmod 751 /usr/local/bin/docker-compose
-sudo curl -L https://github.com/docker/machine/releases/download/v0.6.0-rc2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \
+sudo sh -c "curl -L https://github.com/docker/machine/releases/download/v0.6.0-rc2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine"
 sudo chmod 751 /usr/local/bin/docker-machine
