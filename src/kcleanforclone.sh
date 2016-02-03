d64ba897a16b536af11bd9b07880ea9309f13430
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-02 20:40:13 -0600

    Update kcleanforclone.sh

--- src/kcleanforclone.sh
+++ src/kcleanforclone.sh
@@ -4,5 +4,9 @@ kprovision kalan --remove
 kalan-clean-docker
 sudo rm -rf $KALAN_DIR-data/downloads/*
 sudo rm -rf $KALAN_DIR-data/build/*
+sudo rm -rf $KALAN_DIR-data/provisions/kalan/kalan-container/k-w2p/applications/SG/errors
+sudo rm -rf $KALAN_DIR-data/provisions/kalan/kalan-container/k-w2p/applications/SG/sessions
+sudo rm -rf $KALAN_DIR-data/provisions/kalan/kalan-container/k-w2p/applications/dlintec/errors
+sudo rm -rf $KALAN_DIR-data/provisions/kalan/kalan-container/k-w2p/applications/dlintec/sessions
 rm -f $KALAN_DIR-data/conf/updates
 echo "environment cleaned for clone"
