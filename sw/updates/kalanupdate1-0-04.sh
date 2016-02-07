15e325859a9841ac1535b67c1f9e139cf0874d6a
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-06 22:43:26 -0600

    Update kalanupdate1-0-04.sh

--- sw/updates/kalanupdate1-0-04.sh
+++ sw/updates/kalanupdate1-0-04.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Update 4: This update installs meteor and creates new images for cantainerized meteor app
+#Update 4: This update installs new docker version, installs meteor and creates new images for cantainerized meteor app. Also reconfigures firewall tu forward docker's virtual network. 
 sudo service docker stop
 kalan-install-docker
 sudo service docker start
