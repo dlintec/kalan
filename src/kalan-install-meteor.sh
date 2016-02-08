d7513c3271634825b856ad7d4297daab8aa8317d
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-08 10:52:30 -0600

    Update kalan-install-meteor.sh

--- src/kalan-install-meteor.sh
+++ src/kalan-install-meteor.sh
@@ -3,12 +3,11 @@ sudo apt-get -y install npm curl
 
 curl https://install.meteor.com/ | sh
 curl http://www.meteorkitchen.com/install | /bin/sh
+sudo npm install npm -g
 sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
 sudo npm install -g orion-cli
-
-
 sudo ln -sf /usr/local/n/versions/node/5.4.1/bin/node /usr/bin/node 
 
 
