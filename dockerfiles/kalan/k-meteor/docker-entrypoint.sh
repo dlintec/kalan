df579caccac66613136e37c4625fda433092e56a
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-08 22:32:20 -0600

    Update docker-entrypoint.sh

--- dockerfiles/kalan/k-meteor/docker-entrypoint.sh
+++ dockerfiles/kalan/k-meteor/docker-entrypoint.sh
@@ -4,7 +4,8 @@ par2="$2"
 par3="$3"
 container_sslfolder="/var/kalan-container/ssl"
 set -e
-if [[ ( ! -d /opt/application/private ) ]];then
+if [[ ( ! -e /opt/application/.mustexist ) ]];then
+  echo "#if this file is missing meteor will create a new default app" > /opt/application/.mustexist
   cd /opt
   meteor create application
   cd application 
