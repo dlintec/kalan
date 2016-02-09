a39d3b25624092046f3d29d67f9a0a7b121a745b
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-08 18:41:19 -0600

    Update kalanupdate1-0-04.sh

--- sw/updates/kalanupdate1-0-04.sh
+++ sw/updates/kalanupdate1-0-04.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Update 4: This update installs new docker version, installs meteor and creates new images for cantainerized meteor app. Also reconfigures firewall to forward docker's virtual network. 
 kalan-install-meteor
-kalan-install-conky
 sudo service docker stop
 kalan-install-docker
 sudo service docker start
 kalan-autostart
+kalan-install-conky
 echo "Finished"
