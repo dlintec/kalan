d4b66a65883d0150a61ed2ea615d77bc225117b1
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-10 10:02:17 -0600

    Update kalanupdate1-0-04.sh

--- sw/updates/kalanupdate1-0-04.sh
+++ sw/updates/kalanupdate1-0-04.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-#Update 4: This update installs new docker version, installs meteor and creates new images for "containerized" meteor app. Also reconfigures firewall to forward docker's virtual network. Container Images must be recreated so this may take from 10 to 20 minutes depending on your bandwidth. 
-KALAN_DIR="$HOME/kalan"
-
-sudo usermod -aG docker ${USER}
-sudo usermod -aG sudo ${USER}
-kalan-stop
-kalan-clean-docker
-rm -f $KALAN_DIR-data/provisions/kalan/images/*
-rm -rf $KALAN_DIR-data/provisions/kalan/mongo
-#kalan-install-meteor
-sudo service docker stop
-kalan-install-docker
-sudo service docker start
-kalan-autostart
+#Update 4: This update will install conky desktop monitor. Remember to activate (turn autostart ON) in conky-manager to load conky at startup  
+echo "-----------------------------------------------------"
+echo "  Select the widgets you want to appear in your desktop from conky-manager"
+echo "  Selecciona los elementos de monitoreo en concky manager. "
 kalan-install-conky
 echo "Finished"
