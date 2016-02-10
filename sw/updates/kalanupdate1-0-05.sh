cc09a9b87ae37fe3e2c02caf0e535d4be984a828
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-10 10:19:26 -0600

    Update kalanupdate1-0-05.sh

--- sw/updates/kalanupdate1-0-05.sh
+++ sw/updates/kalanupdate1-0-05.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Update 4: This update installs new docker version, installs meteor and creates new images for "containerized" meteor app. Also reconfigures firewall to forward docker's virtual network. Container Images must be recreated so this may take from 10 to 20 minutes depending on your bandwidth. 
+#Update 4: This update will install new docker version and create new images for "containerized" meteor/nodejs . Also reconfigures firewall to forward docker's virtual network. Container Image creation may take from 10 to 20 minutes depending on your bandwidth. Be patient...
 KALAN_DIR="$HOME/kalan"
 
 sudo usermod -aG docker ${USER}
