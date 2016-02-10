eb2267ab9269e5769e191a0dc25d7c0e256c2477
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-10 12:00:24 -0600

    Update kalan-install-conky.sh

--- src/kalan-install-conky.sh
+++ src/kalan-install-conky.sh
@@ -1,9 +1,10 @@
 sudo apt-add-repository -y ppa:teejee2008/ppa
 sudo apt-get update
 sudo apt-get -y install conky conky-manager
-
-echo "-----------------------------------------------------"
+clear
+echo "-------------------------------------------------------------------------"
 echo "  I will open conky-manager for you..."
+echo "-------------------------------------------------------------------------"
 echo "  a)Select the widgets you want to appear in your desktop."
 echo "    Recommended: second from top-'conky_seamod'"
 echo "  b)Click the equalizer button at the top right (Applications Setting)"
