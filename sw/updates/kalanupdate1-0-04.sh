4f04c3ef35fb6755aef2333a05fc792a6258550c
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-10 10:17:01 -0600

    Update kalanupdate1-0-04.sh

--- sw/updates/kalanupdate1-0-04.sh
+++ sw/updates/kalanupdate1-0-04.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Update 4: This update will install conky desktop monitor. I will open conky-manager for you. a)Select the widgets you want to appear in your desktop . b) click the equalizer button at the top right (Applications Setting) and Activate "Run Conky at system startup"  to load conky at startup  
+#Update 4: This update will install conky desktop monitor. I will open conky-manager for you. Follow th instructions in the console window... 
 echo "-----------------------------------------------------"
 echo "  Select the widgets you want to appear in your desktop from conky-manager"
 echo "  Selecciona los elementos de monitoreo en concky manager. "
