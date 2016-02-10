35dd89831d149549b5871cc1e653850f3f560a68
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-10 10:57:54 -0600

    Update kalan-install-conky.sh

--- src/kalan-install-conky.sh
+++ src/kalan-install-conky.sh
@@ -8,5 +8,5 @@ echo "  a)Select the widgets you want to appear in your desktop."
 echo "    Recommended: second from top-'conky_seamod'"
 echo "  b)Click the equalizer button at the top right (Applications Setting)"
 echo "  c)Activate (to ON) 'Run Conky at system startup'  and click OK"
-{conky-manager} &> /dev/null
+conky-manager &> /dev/null
 
