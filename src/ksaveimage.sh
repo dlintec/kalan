be64a24379192cd4d6aee493ec46818510b04d64
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-11 17:11:28 -0600

    Update ksaveimage.sh

--- src/ksaveimage.sh
+++ src/ksaveimage.sh
@@ -6,6 +6,6 @@ ptag="$3"
 if [[ ( -n "$ptag" ) ]];then
   ptag="latest"
 fi
-docker save --output $KALAN_DIR-data/provisions/$pprovision/images/$pimage.tar $pimage:$ptag
+docker save --output $KALAN_DIR-data/provisions/$pprovision/images/$pimage.tar $pimage/$ptag
 RESULT=$?
 return $RESULT
