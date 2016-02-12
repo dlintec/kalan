72be675ee8bd12e27d39cde307f6a79199986bd3
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-12 00:52:35 -0600

    Update ksyncmeteor.sh

--- src/ksyncmeteor.sh
+++ src/ksyncmeteor.sh
@@ -2,5 +2,5 @@
 KALAN_DIR="$HOME/kalan"
 pprovision="$1"
 cp -rf $KALAN_DIR/dockerfiles/kalan/k-meteor/files $KALAN_DIR-data/provisions/kalan/met/
-sudo docker run -it kalan_meteor meteor-kitchen start.json start
+krefreshmeteor
 
