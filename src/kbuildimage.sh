cca6bc21fd0a3c9b0b1a65c1905058dce4e75092
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-12 00:00:03 -0600

    Update kbuildimage.sh

--- src/kbuildimage.sh
+++ src/kbuildimage.sh
@@ -1,38 +1,25 @@
 #!/bin/bash
-#~/kalan/src/kbuild.sh
+#~/kalan/src/kbuildimage.sh
 main() {
-   dockerfile="$1";shift;
-   param2="$1";shift;
+   pdockerfile="$1";shift;
+   pimagename="$1";shift;
    KALAN_DIR="$HOME/kalan"
-   dir_df="$KALAN_DIR/dockerfiles/$dockerfile"
-   if sudo docker history -q $dockerfile 2>&1 >/dev/null; then
-    echo "image $dockerfile exists"
-   else
-    echo "image $dockerfile does not exist"
+   
+   if [[ -z "$pdockerfile"]];then
+      pdockerfile="$(pwd)/dockerfile"
    fi
-   if [[ -d $dir_df ]];then
-      cd $dir_df
-      sudo docker build -t $dockerfile .
+   if [[ -e $pdockerfile ]];then
+      sudo docker build -t $pimagename $pdockerfile
       RESULT=$?
       if [ $RESULT -eq 0 ]; then
-        img_dir="$KALAN_DIR-data/docker-images"
-        if [[ ! -d $img_dir ]];then
-           mkdir -p $img_dir
-        fi
-        if [[ ! -e $img_dir/kalan-base.tar ]];then
-            echo "Saving kalan-base image en $img_dir "
-            sudo docker save -o $img_dir/kalan-base.tar ubuntu
-        fi
-        echo "please wait...saving image to $img_dir/$dockerfile.tar "
-        sudo docker save -o $img_dir/$dockerfile.tar $dockerfile
         echo success
       else
         echo failed
       fi
    else
-      echo "Must provide a valid folder name in $KALAN_DIR/dockerfiles with valid dockerfile"
-      echo "folder:$dockerfile"
-      echo "$param2"
+      echo "Must provide a valid folder name with valid dockerfile"
+      echo "folder:$pdockerfile"
+      echo "$pimagename"
    fi
 }
 
