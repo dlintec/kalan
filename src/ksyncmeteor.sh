8830e1e800b00e9ff5aa07fc45f7bf0a9a531cd5
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-12 01:10:32 -0600

    Update ksyncmeteor.sh

--- src/ksyncmeteor.sh
+++ src/ksyncmeteor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 KALAN_DIR="$HOME/kalan"
 pprovision="$1"
+mkdir -p $KALAN_DIR-data/provisions/kalan/met
 cp -rf $KALAN_DIR/dockerfiles/kalan/k-meteor/files $KALAN_DIR-data/provisions/kalan/met/
 cp -rf $KALAN_DIR/dockerfiles/kalan/k-meteor/start.json $KALAN_DIR-data/provisions/kalan/met/start.json
 
