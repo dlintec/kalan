d4e6055b9521c63d7d39a1edbf4fff6357e8d77f
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-02-28 15:36:44 -0600

    Update kalan-install-mongochef.sh

--- src/kalan-install-mongochef.sh
+++ src/kalan-install-mongochef.sh
@@ -1,1 +1,9 @@
-https://cdn.3t.io/mongochef/linux/3.4.1/mongochef-linux-x64-dist.tar.gz
+#!/bin/bash
+KALAN_DIR="$HOME/kalan" 
+current_dir=`pwd` 
+echo "installing mongochef..."
+cd $KALAN_DIR-data/downloads
+wget https://cdn.3t.io/mongochef/linux/3.4.1/mongochef-linux-x64-dist.tar.gz
+tar -xvzf mongochef-linux-x64-dist.tar.gz
+./mongochef-3.4.1-linux-x64-dist/bin/mongochef.sh
+
