37fe232e6309d336445de386606cf685b8ccf611
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-07-19 23:28:57 -0500

    Delete kalanupdate1-0-04.sh

--- sw/updates/kalanupdate1-0-04.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Update 4: This update will install conky desktop monitor. I will open conky-manager for you. Follow th instructions in the console window... 
-echo "-----------------------------------------------------"
-echo "  Select the widgets you want to appear in your desktop from conky-manager"
-echo "  Selecciona los elementos de monitoreo en concky manager. "
-kalan-install-conky
-echo "Finished"
