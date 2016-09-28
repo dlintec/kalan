b7faef47fe5884b034d7a64edb15e17b26d1fc1c
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-09-28 13:01:30 -0500

    updates folder fix 2

--- src/kalan-update.sh
+++ src/kalan-update.sh
@@ -81,11 +81,11 @@ if [ -n "$(command -v git)" ]; then
    sudo chmod -R 755 $HOME/Desktop/kalan-update.desktop
    sudo cp -a $KALAN_DIR/src/kalan-update.sh /usr/local/bin/
    gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/kalan-blue.jpg
-   if [[ ! -d $KALAN_DIR-data/conf/updates ]];then
-     mkdir -p $KALAN_DIR-data/conf/updates
+   if [[ ! -d $KALAN_DIR-data/sw/updates ]];then
+     mkdir -p $KALAN_DIR-data/sw/updates
    fi
 
-   echo "added by kalan-update" >> $KALAN_DIR-data/conf/updates
+   echo "added by kalan-update" >> $KALAN_DIR-data/sw/updates
    if [[ ! -e $KALAN_DIR-data/conf/applied-updates ]];then
       echo "First update" > $KALAN_DIR-data/conf/applied-updates
    fi
